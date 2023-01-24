e9792d80f6cedeb42d39e40ca7326451be7868ff
Lakshit <lrsbudhsingh@gmail.com>
2023-01-24 23:03:24 +0530

    Create Audible Range.py

---
+++ Audible Range.py
@@ -0,0 +1,10 @@
+# https://www.codechef.com/problems/AUDIBLE
+# cook your dish here
+
+t = int(input())
+for i in range(t):
+    t1 = int(input())
+    if(67<= t1 <= 45000):
+        print("Yes")
+    else:
+        print("No")
