43d83dc40cec2a2389c0d9727a8508e6dbdbd744
Lakshit <lrsbudhsingh@gmail.com>
2023-01-25 22:00:03 +0530

    Create Parity.py

---
+++ Parity.py
@@ -0,0 +1,8 @@
+# https://www.codechef.com/problems/PAR2
+# cook your dish here
+
+for i in range(int(input())):
+    if(int(input()) % 2 == 0):
+        print("Yes")
+    else:
+        print("No")
