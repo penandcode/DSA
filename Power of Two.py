29bbe4026b035e0bb1b975a3533505a38535f666
Lakshit <lrsbudhsingh@gmail.com>
2023-02-04 23:09:39 +0530

    Create Power of Two.py

---
+++ Power of Two.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/power-of-two/
+
+class Solution(object):
+    def isPowerOfTwo(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if(n == 0):
+            return False
+        return n&(n-1) == 0
