2238668810b28c5fabc0007ab1f0da91e444c7e7
Lakshit <lrsbudhsingh@gmail.com>
2023-02-06 23:45:44 +0530

    Create Best Time to Buy and Sell Stock.py

---
+++ Best Time to Buy and Sell Stock.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        left = 0 #Buy
+        right = 1 #Sell
+        max_profit = 0
+        while right < len(prices):
+            currentProfit = prices[right] - prices[left] #our current Profit
+            if prices[left] < prices[right]:
+                max_profit =max(currentProfit,max_profit)
+            else:
+                left = right
+            right += 1
+        return max_profit
