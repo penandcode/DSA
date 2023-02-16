590219861cd0937cb1d162c34f201fdc9a15ee17
Lakshit <lrsbudhsingh@gmail.com>
2023-02-16 21:39:01 +0530

    Added one more solution.

--- Climbing Stairs.py
+++ Climbing Stairs.py
@@ -13,3 +13,14 @@ class Solution(object):
                 return 2
             return climb(n-1) + climb(n-2)
         return climb(n)
+    
+    class Solution(object):
+        def climbStairs(self, n):
+            """
+            :type n: int
+            :rtype: int
+            """
+            a, b = 0, 1
+            for i in range(1, n+1):
+                a, b = b, b+a
+            return b
