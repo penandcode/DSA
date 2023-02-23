4518ae4c500a44f2ad6a370dcbeab1374edb2492
Lakshit <lrsbudhsingh@gmail.com>
2023-02-23 22:07:30 +0530

    Create Valid Parentheses.py

---
+++ Valid Parentheses.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/valid-parentheses/
+
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        while "()" in s or "{}" in s or "[]" in s:
+            s = s.replace("()", "")
+            s = s.replace("[]", "")
+            s = s.replace("{}", "")
+        if(s == ""):
+            return True
+        else:
+            return False
